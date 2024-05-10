b99e011947fd4bb6142960aa277b4819dabf6803
Stefan Strömer <stefan.stroemer@ait.ac.at>
2024-05-10 12:52:30 +0200

    feat: add functionality to "pretty print" variable/constraint names with a fixed length

--- src/util/util.jl
+++ src/util/util.jl
@@ -19,3 +19,28 @@ macro critical(msg, args...)
         error($msg)
     end)
 end
+
+function _norm_strname(str::String, width::Int64 = 50)
+    textwidth(str) <= width && return rpad(str, width)
+
+    prefix_length = div(width - 3, 2) + div(width - 3, 10)
+    suffix_length = width - prefix_length - 3
+    prefix = ""
+    suffix = ""
+
+    for i in 1:length(str)
+        if textwidth(str[1:prevind(str, i)]) >= prefix_length
+            prefix = str[1:prevind(str, i)]
+            break
+        end
+    end
+
+    for i in length(str):-1:1
+        if textwidth(str[nextind(str, i):end]) >= suffix_length
+            suffix = str[nextind(str, i):end]
+            break
+        end
+    end
+
+    return "$(prefix)...$(suffix)"
+end
