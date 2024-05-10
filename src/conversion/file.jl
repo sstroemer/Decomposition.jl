12baf048a32e03cc68fb82adfb4c8e55e56a1088
Stefan Strömer <stefan.stroemer@ait.ac.at>
2024-05-11 00:16:17 +0200

    feat: keep objective sense in MPS files; allow keeping explicit names when writing to file

--- src/conversion/file.jl
+++ src/conversion/file.jl
@@ -1,17 +1,17 @@
 from_file(filename::String) = Node{FileNodeGeneral}(Node{NodeRoot}(); filename=filename)
 
-function to_file(node::AbstractNode, filename::String = "")
+function to_file(node::AbstractNode, filename::String = ""; generic_names::Bool = false)
     @debug "to_file(::AbstractNode, ::String)" node filename
     node = to_model(node)
     (filename == "") && (filename = "$(tempname()).mps")
-    JuMP.write_to_file(node.model, filename)
+    JuMP.write_to_file(node.model, filename; print_objsense=true, generic_names=generic_names)
     return Node{FileNodeGeneral}(node; filename=filename)
 end
 
-function to_file(node::AbstractModelNode, filename::String = "")
+function to_file(node::AbstractModelNode, filename::String = ""; generic_names::Bool = false)
     @debug "to_file(::AbstractModelNode, ::String)" node filename
     (filename == "") && (filename = "$(tempname()).mps")
-    JuMP.write_to_file(node.model, filename)
+    JuMP.write_to_file(node.model, filename; print_objsense=true, generic_names=generic_names)
     return Node{FileNodeGeneral}(node; filename=filename)
 end
 
