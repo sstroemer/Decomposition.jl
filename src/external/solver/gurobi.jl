b44aaba91e32384c7640d4f9854a8d8f4ebab558
sstroemer <stefan.stroemer@gmail.com>
2024-09-02 16:41:58 +0200

    not working changes to dual ray extraction

--- src/external/solver/gurobi.jl
+++ src/external/solver/gurobi.jl
@@ -19,5 +19,11 @@ end
 function _gurobi_modify_jump(jump_model::JuMP.Model, attribute::ExtractDualRay)
     JuMP.set_attribute(jump_model, "InfUnbdInfo", attribute.activate ? 1 : 0)
     JuMP.set_attribute(jump_model, "DualReductions", attribute.activate ? 0 : 1)
+
+    if attribute.activate && JuMP.get_attribute(jump_model, "Crossover") == 0
+        @warn "`ExtractDualRay` requires crossover to be enabled, overwriting the previous setting"
+        JuMP.set_attribute(jump_model, "Crossover", -1)
+    end
+
     return true
 end
--- src/external/solver/highs.jl
+++ src/external/solver/highs.jl
@@ -22,6 +22,11 @@ end
 function _highs_modify_jump(jump_model::JuMP.Model, attribute::ExtractDualRay)
     JuMP.set_attribute(jump_model, "presolve", attribute.activate ? "off" : "on")
 
+    if attribute.activate && JuMP.get_attribute(jump_model, "run_crossover") == "off"
+        @warn "`ExtractDualRay` requires crossover to be enabled, overwriting the previous setting"
+        JuMP.set_attribute(jump_model, "run_crossover", "on")
+    end
+
     # TODO: does that also need "sovler=simplex" and "simplex_strategy=dual"?
     # See:  https://ampl.com/colab/notebooks/ampl-development-tutorial-56-parallelizing-subproblem-solves-in-benders-decomposition.html#utility-and-worker-functions
     # set_attribute(jump_model, "solver", activate ? "simplex" : "choose")
