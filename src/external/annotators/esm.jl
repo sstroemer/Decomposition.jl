d665ba9b4588b742c426adb5a5fc8c9658a26474
sstroemer <stefan.stroemer@gmail.com>
2025-03-03 22:52:00 +0100

    allow saving annotations to disk

--- src/external/annotators/esm.jl
+++ src/external/annotators/esm.jl
@@ -3,7 +3,19 @@ abstract type AbstractExternalESMAnnotator <: AbstractGeneralAnnotator end
 include("esm/calliope.jl")
 
 function _call_annotator(model::AbstractDecomposedModel, annotator::AbstractExternalESMAnnotator)
-    _generate_annotations(model, annotator)
+    T = get_attribute(model, Benders.Config.TotalTimesteps, :T)
+    n = get_attribute(model, Benders.Config.NumberOfTemporalBlocks, :n)
+    annotation_file = "$(model.name)_T$(T)_n$(n).djl.json"
+
+    if isfile(annotation_file)
+        @info "Loading annotations from file" annotation_file
+        annotations = JSON3.read(annotation_file, Dict{Symbol, Dict{Symbol, Vector{Int64}}})
+        merge!(model.annotations, annotations)
+    else
+        _generate_annotations(model, annotator)
+        @info "Saving annotations to file" annotation_file
+        JSON3.write(annotation_file, model.annotations)
+    end
 
     # Un-dirty all related attributes.
     for ac in model.attributes
