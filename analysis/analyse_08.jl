5f8f05d09f8bead137377a52efe543f10dcef58f
Stefan Strömer <stefan.stroemer@ait.ac.at>
2025-03-12 17:06:59 +0100

    experiment 08 added

---
+++ analysis/analyse_08.jl
@@ -0,0 +1,91 @@
+import JSON3
+using Statistics: mean
+using PlotlyJS: PlotlyJS, plot, Layout, savefig, scatter, bar
+
+const EXPERIMENT_NR = split(split(basename(@__FILE__), ".")[1], "_")[2]
+const RESULT_DIR = normpath(@__DIR__, "..", "experiments", "out")
+const RUN_DIR = normpath(RESULT_DIR, only(filter(it -> startswith(it, "$(EXPERIMENT_NR)"), readdir(RESULT_DIR))))
+const RUNS = filter(x -> isdir(joinpath(RUN_DIR, x)), readdir(RUN_DIR))
+hcomb(a, b) = isnothing(a) ? b : hcat(a, b)
+
+const exp_tol = collect(0:10)
+const tol = vcat([0.0], 10.0 .^ (-exp_tol[2:end]))
+
+y = Dict(i => Dict{String, Any}("iter" => nothing, "time" => nothing, "lb" => nothing, "ub" => nothing) for i in exp_tol)
+
+# Extract results.
+for r in RUNS
+    dir = joinpath(RUN_DIR, r)
+
+    for i in exp_tol
+        history = JSON3.read(joinpath(dir, "history_$(i).json"))
+        # timings = JSON3.read(joinpath(dir, "timer_$(i).json"))
+
+        y[i]["iter"] = [h["k"] for h in history]
+
+        t0 = history[1]["t"]["timestamp"]
+        y[i]["time"] = hcomb(y[i]["time"], [(h["t"]["timestamp"] - t0) for h in history])
+
+        lb, ub = [-Inf], [+Inf]
+        for j in eachindex(history)
+            push!(lb, max(lb[end], history[j]["lb"]))
+            push!(ub, min(ub[end], history[j]["ub"]))
+        end
+        y[i]["lb"] = hcomb(y[i]["lb"], lb[2:end])
+        y[i]["ub"] = hcomb(y[i]["ub"], ub[2:end])
+    end
+end
+
+# Average results and convert to seconds.
+for i in exp_tol
+    y[i]["time"] = vec(mean(y[i]["time"]; dims = 2)) ./ 1e9
+    y[i]["lb"] = vec(mean(y[i]["lb"]; dims = 2))
+    y[i]["ub"] = vec(mean(y[i]["ub"]; dims = 2))
+end
+
+# Precalculate relative gap.
+for i in exp_tol
+    y[i]["gap"] = (y[i]["ub"] - y[i]["lb"]) ./ y[i]["ub"]
+    y[i]["gap_reached"] = [findfirst(<=(th), y[i]["gap"]) for th in [0.1, 0.05, 0.025, 0.01]]
+end
+
+# Plot.
+function make_plot(traces, layout)
+    kwlay = Dict(
+        :title => "",
+        :xaxis => PlotlyJS.attr(; showgrid = true, zeroline = false, showline = true, mirror = true, ticks = "outside", ticklen = 5, tickwidth = 1.5, tickcolor = "black", gridcolor = "lightgray"),
+        :yaxis => PlotlyJS.attr(; showgrid = true, zeroline = false, showline = true, mirror = true, ticks = "outside", ticklen = 5, tickwidth = 1.5, tickcolor = "black", gridcolor = "lightgray"),
+        :legend => PlotlyJS.attr(; x = 0.10, y = 0.95, bordercolor = "black", borderwidth = 1, xanchor = "left", yanchor = "top", orientation = "h"),
+        :plot_bgcolor => "white",
+        :paper_bgcolor => "white",
+        :font => PlotlyJS.attr(; family = "Arial, sans-serif", size = 10, color = "black"),
+        :margin => PlotlyJS.attr(; l = 80, r = 50, b = 65, t = 90),
+    )
+
+    return plot(traces, Layout(; kwlay..., layout...))
+end
+
+opacity = ["ff", "cf", "b0", "9f"]
+
+iter_max = maximum(y[i]["iter"][end] for i in exp_tol)
+time_max = maximum(y[i]["time"][end] for i in exp_tol)
+base_iter = y[0]["iter"][end] / 100.0
+base_time = y[0]["time"][end] / 100.0
+ymax = max(iter_max / base_iter, time_max / base_time) * 1.05
+x = [(e == 0) ? "" : "1e-$(e)" for e in exp_tol]
+
+traces = Vector{PlotlyJS.GenericTrace}()
+push!(traces, scatter(; zorder = -1, x = x, y = fill(100.0, length(x)), mode = "lines", showlegend = false, line = PlotlyJS.attr(; color = "black", dash = "dash")))
+for (i, gr) in enumerate(y[0]["gap_reached"])
+    push!(traces, bar(; zorder = -2, x = x, y = vcat(y[0]["iter"][gr] / base_iter, zeros(length(exp_tol) - 1)), marker_color = "#ff4800$(opacity[i])", legendgrouptitle = PlotlyJS.attr(; text = "concurrent"), legendgroup = "concurrent", name = "Δ = $(["10%", "5%", "2.5%", "1%"][i])"))
+    push!(traces, bar(; zorder = 1, x = x, y = vcat(0.0, [y[e]["iter"][y[e]["gap_reached"][i]] / base_iter for e in exp_tol[2:end]]), marker_color = "#0026ff$(opacity[i])", legendgrouptitle = PlotlyJS.attr(; text = "barrier"), legendgroup = "barrier", name = "Δ = $(["10%", "5%", "2.5%", "1%"][i])"))
+end
+savefig(make_plot(traces, (barmode = "overlay", xaxis_title = "barrier tolerance", yaxis_title = "iterations (%)", yaxis_range = [0, ymax])), joinpath(RUN_DIR, "iterations.svg"); width = 700, height = 550)
+
+traces = Vector{PlotlyJS.GenericTrace}()
+push!(traces, scatter(; zorder = -1, x = x, y = fill(100.0, length(x)), mode = "lines", showlegend = false, line = PlotlyJS.attr(; color = "black", dash = "dash")))
+for (i, gr) in enumerate(y[0]["gap_reached"])
+    push!(traces, bar(; zorder = -2, x = x, y = vcat(y[0]["time"][gr] / base_time, zeros(length(exp_tol) - 1)), marker_color = "#ff4800$(opacity[i])", legendgrouptitle = PlotlyJS.attr(; text = "concurrent"), legendgroup = "concurrent", name = "Δ = $(["10%", "5%", "2.5%", "1%"][i])"))
+    push!(traces, bar(; zorder = 1, x = x, y = vcat(0.0, [y[e]["time"][y[e]["gap_reached"][i]] / base_time for e in exp_tol[2:end]]), marker_color = "#0026ff$(opacity[i])", legendgrouptitle = PlotlyJS.attr(; text = "barrier"), legendgroup = "barrier", name = "Δ = $(["10%", "5%", "2.5%", "1%"][i])"))
+end
+savefig(make_plot(traces, (barmode = "overlay", xaxis_title = "barrier tolerance", yaxis_title = "time (%)", yaxis_range = [0, ymax])), joinpath(RUN_DIR, "time.svg"); width = 700, height = 550)
---
+++ experiments/08_inexact_oracles.jl
@@ -0,0 +1,85 @@
+# Run N times:
+# julia --project=experiments experiments/08_inexact_oracles.jl
+
+import TimerOutputs, JSON3, UUIDs
+import JuMP, HiGHS, Gurobi
+using Decomposition
+
+
+const GRB_ENV = Gurobi.Env()
+const EXPERIMENT = split(basename(@__FILE__), ".")[1]
+const EXPERIMENT_UUID = string(UUIDs.uuid4())
+const RESULT_DIR = mkpath(joinpath(@__DIR__, "out", EXPERIMENT, EXPERIMENT_UUID))
+
+
+function experiment(jump_model::JuMP.Model; T::Int64, n::Int64, tol::Float64)
+    model = Benders.DecomposedModel(;
+        jump_model,
+        annotator = Calliope(),
+        f_opt_main = () -> Gurobi.Optimizer(GRB_ENV),
+        f_opt_sub = () -> Gurobi.Optimizer(GRB_ENV),
+    )
+
+    set_attribute.(
+        model,
+        [
+            Benders.Config.TotalTimesteps(T),
+            Benders.Config.NumberOfTemporalBlocks(n),
+            Benders.Config.ModelVerbosity(1),
+            Benders.Config.ModelDirectMode(; enable = false),
+            Solver.AlgorithmIPM(; model = :main),
+            Benders.OptimalityCutTypeMulti(),
+            Benders.Sub.RelaxationLinked(; penalty = 1e6),
+            Benders.Main.VirtualSoftBounds(0.0, 1e6),
+            Benders.Main.ObjectiveDefault(),
+            Benders.Main.RegularizationLevelSet(alpha = 0.5, infeasible_alpha_step = 0.2),
+            Benders.Termination.Stop(; opt_gap_rel = 1e-2, iterations = 1000),
+        ],
+    )
+
+    finalize!(model)
+
+    # Explicitly set method/crossover/tolerance here to make it verbose.
+    cutoff = JuMP.num_variables(model.models[2]) * 0.5
+    for sm in Benders.subs(model)
+        if (tol > 0) && (JuMP.num_variables(sm) > cutoff)
+            JuMP.set_attribute(sm, "Method", 2)
+            JuMP.set_attribute(sm, "Crossover", 0)
+            JuMP.set_attribute(sm, "BarConvTol", tol)
+        else
+            JuMP.set_attribute(sm, "Method", 3)
+        end
+    end
+
+    while !iterate!(model; nthreads = -1)
+    end
+
+    return model
+end
+
+# Make sure everything's compiled using a small model first.
+experiment(jump_model_from_file("national_scale_120.mps"); T = 120, n = 2, tol = 0.0)
+experiment(jump_model_from_file("national_scale_120.mps"); T = 120, n = 2, tol = 1e-5)
+
+# Load JuMP model.
+jump_model = jump_model_from_file("national_scale_744.mps")
+
+# Now run the experiment.
+for i in 0:10
+    println("Running experiment: $(EXPERIMENT) >> $(EXPERIMENT_UUID) >> $(i)")
+    model = experiment(jump_model; T = 744, n = 2, tol = (i == 0) ? 0.0 : 10.0 ^ (-i))
+
+    # Write results.
+    JSON3.write(joinpath(RESULT_DIR, "timer_$(i).json"), TimerOutputs.todict(model.timer))
+    JSON3.write(
+        joinpath(RESULT_DIR, "history_$(i).json"),
+        [
+            Dict(
+                "k" => (entry[:iteration] + 1),
+                "t" => entry[:time],
+                "lb" => entry[:lower_bound],
+                "ub" => entry[:upper_bound],
+            ) for entry in model.info[:history]
+        ],
+    )
+end
